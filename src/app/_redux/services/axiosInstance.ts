9b24810d978dc746d3c0e77d82a8bcc2b41df813
alaahosnii <alaahosnii1000@gmail.com>
2025-09-03 20:07:19 +0300

    Refactor axios instance configuration by removing environment-based backend URL and simplifying base URL to `/api`.

--- src/app/_redux/services/axiosInstance.ts
+++ src/app/_redux/services/axiosInstance.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
-
-const BACKEND_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.NEXT_PUBLIC_BACKEND_URL;
 const instance = axios.create({
-    baseURL: `${BACKEND_URL}/api`,
+    baseURL: `/api`,
     headers: {
         "Content-Type": "application/json",
     },
